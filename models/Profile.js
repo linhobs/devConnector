946a76bd57c92395d7b7f0b6718c08bbb3a01b04
Pius <gyamenahyawpius@gmail.com>
2019-11-11 14:43:35 +0000

    added profile functionality

--- models/Profile.js
+++ models/Profile.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
+const User=require('./User');
+
 const Schema = mongoose.Schema;
 
 // Create Schema
 const ProfileSchema = new Schema({
   user: {
     type: Schema.Types.ObjectId,
-    ref: 'users'
+    ref: 'user'
   },
  
   company: {
